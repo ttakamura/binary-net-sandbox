99a7003272c39c17df17d9d8cb3a0d88069fbf20
Tatsuya Takamura <takamura@tonchidot.com>
2016-09-06 18:48:21 +0900

    Use BatchNormalize

--- chainer-binary-net/sample_net.py
+++ chainer-binary-net/sample_net.py
@@ -6,13 +6,17 @@ from chainer.training import extensions
 
 class MLP(chainer.Chain):
     def __init__(self, n_in, n_units, n_out):
-        super(MLP, self).__init__(
-            l1=L.Linear(n_in, n_units),  # first layer
-            l2=L.Linear(n_units, n_units),  # second layer
-            l3=L.Linear(n_units, n_out),  # output layer
-        )
+      super(MLP, self).__init__(
+        l1=L.Linear(n_in, n_units),  # first layer
+        b1=L.BatchNormalization(n_units),
+        l2=L.Linear(n_units, n_units),  # second layer
+        b2=L.BatchNormalization(n_units),
+        l3=L.Linear(n_units, n_out),  # output layer
+        b3=L.BatchNormalization(n_out),
+      )
+      self.train = True
 
     def __call__(self, x):
-        h1 = F.relu(self.l1(x))
-        h2 = F.relu(self.l2(h1))
-        return self.l3(h2)
+      h1 = F.relu(self.b1(self.l1(x), test=not self.train))
+      h2 = F.relu(self.b2(self.l2(h1), test=not self.train))
+      return self.b3(self.l3(h2), test=not self.train)
