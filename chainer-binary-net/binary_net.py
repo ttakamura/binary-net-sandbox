bf3148848aff8b066fc11607d5f06ea9ed531b97
Tatsuya Takamura <takamura@tonchidot.com>
2016-09-06 20:05:42 +0900

    Don't binalize on first layer input

--- chainer-binary-net/binary_net.py
+++ chainer-binary-net/binary_net.py
@@ -9,8 +9,7 @@ from binary_linear import BinaryLinear
 class BinaryMLP(chainer.Chain):
     def __init__(self, n_in, n_units, n_out):
       super(BinaryMLP, self).__init__(
-        b0=L.BatchNormalization(n_in),
-        l1=BinaryLinear(n_in, n_units),  # first layer
+        l1=L.Linear(n_in, n_units),  # first layer
         b1=L.BatchNormalization(n_units),
         l2=BinaryLinear(n_units, n_units),  # second layer
         b2=L.BatchNormalization(n_units),
@@ -20,7 +19,6 @@ class BinaryMLP(chainer.Chain):
       self.train = True
 
     def __call__(self, x):
-      x  = self.b0(x, test=not self.train)
       h1 = self.b1(self.l1(x), test=not self.train)
       h2 = self.b2(self.l2(h1), test=not self.train)
       return self.b3(self.l3(h2), test=not self.train)
