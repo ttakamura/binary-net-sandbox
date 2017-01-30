dc723c18391590615ba2a5cdfa1c9b5bb49fc831
Tatsuya Takamura <takamura@tonchidot.com>
2017-01-31 07:14:27 +0900

    Export batch norm

--- chainer-binary-net/export.py
+++ chainer-binary-net/export.py
@@ -12,12 +12,33 @@ from chainer import training
 from chainer.training import extensions
 from binary_net import BinaryMLP
 
+def pack_vector(vector, d):
+  for val in vector:
+    d += struct.pack('f',val)
+  return d
+
 def write_liner_W(data, filename):
   d = bytearray()
-  data = np.where(data >= 0, 1, -1).astype(np.float32, copy=False)
-  for row in data:
-    for val in row:
-      d += struct.pack('f',val)
+  matrix = np.where(data >= 0, 1, -1).astype(np.float32, copy=False)
+  for row in matrix:
+    d = pack_vector(row, d)
+  with open(filename,'wb') as f:
+    f.write(d)
+
+# >>> repl.model.predictor.b1.avg_mean.shape
+# (1000,)
+# >>> repl.model.predictor.b1.avg_var.shape
+# (1000,)
+# >>> repl.model.predictor.b1.beta.data.shape
+# (1000,)
+# >>> repl.model.predictor.b1.gamma.data.shape
+# (1000,)
+def write_batch_norm(data, filename):
+  d = bytearray()
+  d = pack_vector(data.avg_mean, d)
+  d = pack_vector(data.avg_var, d)
+  d = pack_vector(data.beta.data, d)
+  d = pack_vector(data.gamma.data, d)
   with open(filename,'wb') as f:
     f.write(d)
 
@@ -27,14 +48,10 @@ unit = 1000
 model = L.Classifier(BinaryMLP(784, unit, 10))
 chainer.serializers.load_npz(argvs[1], model)
 
-# --- L1 -----
 write_liner_W(model.predictor.l1.W.data, 'result/binary_net.l1.W.dat')
 write_liner_W(model.predictor.l2.W.data, 'result/binary_net.l2.W.dat')
 write_liner_W(model.predictor.l3.W.data, 'result/binary_net.l3.W.dat')
 
-
-
-predictor/b1/avg_mean.npy
-predictor/b1/avg_var.npy
-predictor/b1/beta.npy
-predictor/b1/gamma.npy
+write_batch_norm(model.predictor.b1, 'result/binary_net.b1.dat')
+write_batch_norm(model.predictor.b2, 'result/binary_net.b2.dat')
+write_batch_norm(model.predictor.b3, 'result/binary_net.b3.dat')
